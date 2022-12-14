3a75bef3280e8ada49de3e28652b949b8e54032d
VilotStar <vilotstarbiss@gmail.com>
2022-12-14 23:06:39 +0000

    Added new arguments and allowed for many threads

--- src/scan.rs
+++ src/scan.rs
@@ -1,7 +1,7 @@
 use mcping::{get_status, Response};
 use std::time::Duration;
 use std:: net::Ipv4Addr;
-use rayon::prelude::*;
+use rayon::{prelude::*, ThreadPoolBuilder};
 use clap::{Parser, arg, command};
 
 use crate::IPHex;
@@ -11,34 +11,42 @@ use crate::IPHex;
 #[command(about = "Scans the interwebs for minecraft servers")]
 #[allow(non_snake_case)]
 pub struct Scan {
-    #[arg(short, long, default_value_t = Ipv4Addr::new(82,176,3,214))]
+    #[arg(short, long, default_value_t = Ipv4Addr::new(82,174,150,98))]
     pub startIp: Ipv4Addr,
+    #[arg(short, long, default_value_t = Ipv4Addr::new(255,255,255,255))]
+    pub endIp: Ipv4Addr,
+    #[arg(short, long, default_value_t = 50)]
+    pub nWorker: u32,
     #[arg(short, long)]
     pub addPorts: Option<Vec<u16>>
 }
 
 impl Scan {
     pub fn scan(&self) {
-        let current_ip: Ipv4Addr = self.startIp;
-        let hex = current_ip.as_hex_number();
-        let jobs = 0..(4294967295 - hex);
+        let start_hex = self.startIp.as_hex_number();
+        let end_hex = self.endIp.as_hex_number();
 
-        jobs.into_par_iter()
-            .for_each(|i| self.do_check(i));
+        let jobs = 0..(end_hex - start_hex);
+        //let pool = ThreadPoolBuilder::new().num_threads(self.nWorker.try_into().unwrap()).build().unwrap();
+        rayon::ThreadPoolBuilder::new().num_threads(self.nWorker.try_into().unwrap()).build_global().unwrap();
+
+        jobs.into_par_iter().for_each(|i| self.do_check(i));
+
+        //jobs.for_each(|i| pool.install(|| self.do_check(i)));
     }
     
     fn do_check(&self, job: u32) {
-        let current_ip: Ipv4Addr = self.startIp;
-        let hex = current_ip.as_hex_number() + job;
+        let hex = self.startIp.as_hex_number() + job;
         let new_ip: Ipv4Addr = Ipv4Addr::from(hex); 
 
         //let (latency, response) = get_status(&new_ip.to_string(), Duration::from_millis(5000));
+
         match get_status(&new_ip.to_string(), Duration::from_millis(5000)) {
             Ok((latency, response)) => {
-                println!("{}|{}: {} {:?}", job, new_ip.to_string(), latency, response.version.name);
+                println!("{}|{}: {} {:?} {:?}", job, new_ip.to_string(), latency, response.version.name, response.players.online);
             },
             Err(_) => {
-                println!("{}|{}: No worky", job, new_ip.to_string());
+                //println!("{}|{}: No worky", job, new_ip.to_string());
             },
         }
     }
